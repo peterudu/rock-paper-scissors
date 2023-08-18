ba0c6ebb7b56e4c95e0e48db85b3ea925a1be561
peterudu <puduevborho7@gmail.com>
2023-08-18 03:02:24 +0000

    Add constants for DOM elements and conduct logs to check their correctness

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+//  Declare constants for DOM elements and possible game choices
+const buttons = document.querySelectorAll(".control");
+const playerScore = document.querySelector("#player-score");
+const computerScore = document.querySelector("#computer-score");
+const playerImage = document.querySelector("#player-image");
+const computerImage = document.querySelector("#computer-image");
+const messages = document.querySelector("#messages");
+const choices = ["rock", "paper", "scissors"];
+
+console.log(buttons)
+console.log(playerScore.innerHTML)
+console.log(computerScore.innerHTML)
+console.log(playerImage)
+console.log(computerImage)
+console.log(messages)
+console.log(choices)
